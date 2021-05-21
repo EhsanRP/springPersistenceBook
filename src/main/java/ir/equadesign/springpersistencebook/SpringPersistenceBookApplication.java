cca46e0e82908e7cbfc9f752c49b6a26dee8898a
ehsan <sir.ehrp@gmail.com>
2021-05-22 00:49:45 +0430

    Project Init

---
+++ src/main/java/ir/equadesign/springpersistencebook/SpringPersistenceBookApplication.java
@@ -0,0 +1,13 @@
+package ir.equadesign.springpersistencebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersistenceBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPersistenceBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/equadesign/springpersistencebook/SpringPersistenceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.equadesign.springpersistencebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersistenceBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
